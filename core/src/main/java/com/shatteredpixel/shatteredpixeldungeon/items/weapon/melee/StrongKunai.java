e915bb47deaaa4dbc5fc02c7e60965501f981e9a
Christian <verylonely@protonmail.ch>
2020-01-02 03:11:08 +0300

    strong kunai class

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/StrongKunai.java
@@ -0,0 +1,15 @@
+package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
+
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class StrongKunai extends MeleeWeapon {
+
+    {
+        image = ItemSpriteSheet.KUNAI;
+
+        tier = 4;
+        ACC = 0.25f;
+
+    }
+
+}
