3aa820db798f636c9678891ea39868bada75165c
Christian <verylonely@protonmail.ch>
2020-01-03 18:03:26 +0300

    added Designer

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AboutScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/AboutScene.java
@@ -40,7 +40,8 @@ public class AboutScene extends PixelScene {
 	private static final String TTL_GPD = "Gloomy Pixel Dungeon";
 
 	private static final String TXT_GPD =
-			"Code: VeryLonely";
+			"Code: VeryLonely\n" +
+			"Design & Graphics: Yorkie-";
 
 	private static final String TTL_SHPX = "Shattered Pixel Dungeon";
 
