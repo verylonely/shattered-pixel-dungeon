5d879f1ea76b045aa8c5bf3ba041192d49bf0bce
Christian <verylonely@protonmail.ch>
2020-01-03 16:41:25 +0300

    added dies sound for Roomless

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Roomless.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Roomless.java
@@ -1,8 +1,15 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Badges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.Statistics;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.RoomlessSprite;
+import com.watabou.noosa.audio.Sample;
 import com.watabou.utils.Random;
 
 public class Roomless extends Mob {
@@ -29,5 +36,30 @@ public class Roomless extends Mob {
     @Override
     public int attackSkill( Char target ) {
         return 10;
+    }
+
+    @Override
+    public void destroy() {
+
+        super.destroy();
+
+        Dungeon.level.mobs.remove( this );
+
+        if (Dungeon.hero.isAlive()) {
+
+            if (alignment == Alignment.ENEMY) {
+                Statistics.enemiesSlain++;
+                Badges.validateMonstersSlain();
+                Statistics.qualifiedForNoKilling = false;
+
+                int exp = Dungeon.hero.lvl <= maxLvl ? EXP : 0;
+                if (exp > 0) {
+                    Dungeon.hero.sprite.showStatus(CharSprite.POSITIVE, Messages.get(this, "exp", exp));
+                }
+                Dungeon.hero.earnExp(exp, getClass());
+            }
+        }
+
+        Sample.INSTANCE.play( Assets.SND_ROOMLESS_DIES );
     }
 }
