dfae17414d6796d6d5161daeca2f75076ee01d70
Christian <verylonely@protonmail.ch>
2020-01-10 11:03:08 +0300

    Customize Zombie

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Zombie.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Zombie.java
@@ -1,6 +1,11 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ZombieSprite;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Random;
 
 public class Zombie extends Mob {
 
@@ -9,6 +14,19 @@ public class Zombie extends Mob {
 
         HP = HT = 20;
         defenseSkill = 5;
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange(1, 6);
+    }
+
+    @Override
+    public int attackSkill(Char target) { return 10; }
+
+    protected boolean canAttack( Char enemy ) {
+        Sample.INSTANCE.play(Assets.SND_ZOMBIE_VOICE);
+        return Dungeon.level.adjacent( pos, enemy.pos );
     }
 
 }
