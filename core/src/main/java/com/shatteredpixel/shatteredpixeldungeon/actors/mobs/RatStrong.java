9d01394aedd7307a3f0f14b10b8ccc19b3fd5b3e
Christian <verylonely@protonmail.ch>
2020-06-12 19:17:01 +0300

    RatStrong: created new mob

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RatStrong.java
@@ -0,0 +1,16 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
+
+import com.shatteredpixel.shatteredpixeldungeon.sprites.RatSprite;
+
+public class RatStrong extends Mob {
+
+    {
+        spriteClass = RatSprite.class;
+
+        HP = HT = 12;
+        defenseSkill = 3;
+
+        maxLvl = 8;
+    }
+
+}
