b9105e82fee7ca1dab135b74786d88810d047a48
Christian <verylonely@protonmail.ch>
2020-06-12 19:18:37 +0300

    RatStrong: reformat base functions

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RatStrong.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/RatStrong.java
@@ -1,6 +1,8 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.RatSprite;
+import com.watabou.utils.Random;
 
 public class RatStrong extends Mob {
 
@@ -11,6 +13,21 @@ public class RatStrong extends Mob {
         defenseSkill = 3;
 
         maxLvl = 8;
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange( 2, 5 );
+    }
+
+    @Override
+    public int attackSkill( Char target ) {
+        return 8;
+    }
+
+    @Override
+    public int drRoll() {
+        return Random.NormalIntRange(0, 1);
     }
 
 }
