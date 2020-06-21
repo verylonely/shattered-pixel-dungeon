4651df7f0ae753a2bfe86a61e51d4afee302ac7f
Christian <verylonely@protonmail.ch>
2020-06-21 19:42:04 +0300

    0.8.1-d: update version

--- desktop/src/com/shatteredpixel/shatteredpixeldungeon/desktop/DesktopLauncher.java
+++ desktop/src/com/shatteredpixel/shatteredpixeldungeon/desktop/DesktopLauncher.java
@@ -67,13 +67,13 @@ public class DesktopLauncher {
 		
 		Game.version = DesktopLauncher.class.getPackage().getSpecificationVersion();
 		if (Game.version == null) {
-			Game.version = "0.8.1-e";
+			Game.version = "0.8.1-d";
 		}
 		
 		try {
 			Game.versionCode = Integer.parseInt(DesktopLauncher.class.getPackage().getImplementationVersion());
 		} catch (NumberFormatException e) {
-			Game.versionCode = 406;
+			Game.versionCode = 407;
 		}
 		
 		new LwjglApplication(new ShatteredPixelDungeon(new DesktopPlatformSupport()), config);
